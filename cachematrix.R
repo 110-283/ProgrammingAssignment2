8a9a129b676e2d6a8a75a7819a7461bf39683ae9
Anton Sinev <sinevav@gmail.com>
2017-08-09 11:13:21 +0700

    Assignment #2 from Anton Sinev

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below ae written by analogy with the vector ones which has been provided as 
+## an example.
 
-## Write a short comment describing this function
+## The "makeCacheMatrix" function creates a matrix and return a list of functions that allow 
+## to operate with the matrix:
+## - get the matrix;
+## - set the matrix (i.e. change the existing matrix);
+## - get the inversed matrix;
+## - set the inversed matrix.
+## The function returns a list of functions described above. These functions allow you to 
+## interact with the matrix object.
+
+## The "cacheSolve" functions calculates the inversed matrix (using "Solve" function) or 
+## takes it from cache if it has already been calculated.
+
+## The "makeCacheMatrix" function defines a matrix object and rules of interaction with it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    iMatrix <- NULL
+    set <- function (y){
+        x <<- y
+        iMatrix <<- NULL
+    }
+    
+    get <- function() {x}
+    setIMatrix <- function (invMatrix) {iMatrix <<- invMatrix}
+    getIMatrix <- function() {iMatrix}
+    
+    list (set = set, get = get, setIMatrix = setIMatrix, getIMatrix = getIMatrix)
 }
 
-
-## Write a short comment describing this function
+## The "cacheSolve" function returns inversed matrix, which was defined by the 
+## "makeCacheMatrix" fucntion.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    iMatrix <- x$getIMatrix()
+    if (!is.null(iMatrix)){
+        message("getting cached data")
+        return(iMatrix)
+    }
+    tempMatrix <- x$get()
+    iMatrix <- solve(tempMatrix,...)
+    x$setIMatrix(iMatrix)
+    iMatrix
 }
+
+## To check correctness of the functions I used the following code:
+
+## sample <- matrix(rnorm(25),ncol = 5)
+## x <- makeCacheMatrix(sample)
+
+## cacheSolve(x)
+## tmp <- cacheSolve(x)
+## tmp
+
+## x$set(matrix(rnorm(36),ncol = 6))
+
+## cacheSolve(x)
+## tmp <- cacheSolve(x)
+## tmp
